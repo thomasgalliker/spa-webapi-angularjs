1cca5828fca3156e7195d15539c8e6350e8e72af
thomasgalliker <thomas@bluewin.ch>
2017-01-24 21:01:40 +0100

    Cleanup

--- HomeCinema.Web/Scripts/spa/services/responseErrorHttpInterceptor.js
+++ HomeCinema.Web/Scripts/spa/services/responseErrorHttpInterceptor.js
@@ -10,13 +10,14 @@
                 'responseError': function (httpResponse) {
                     var $rootScope;
                     var $location;
+                    var title;
+                    var message;
 
                     if (httpResponse.status === 401) {
-
-                        var title = "Authentication required (Error " + httpResponse.status + ")";
-                        var message = "<br/><b>CorrelationId: </b> " + httpResponse.data.CorrelationId +
-                                      "<br/><b>Message: </b>Log in <a href=\"#\login\"\">here</a> to get authenticated." +
-                                      "<br/><br/>";
+                        title = "Authentication required (Error " + httpResponse.status + ")";
+                        message = "<br/><b>CorrelationId: </b> " + httpResponse.data.CorrelationId +
+                                  "<br/><b>Message: </b>Log in <a href=\"#\login\"\">here</a> to get authenticated." +
+                                  "<br/><br/>";
 
                         notificationService.displayError(title, message);
                         $rootScope = $injector.get('$rootScope');
@@ -37,15 +38,13 @@
                         $location.path('/error/404');
                     }
                     else if (httpResponse.status === 500) {
-                        
-                        var title = httpResponse.statusText;
-                        var message = "<small>" +
-                                      "<br/>Error code: <b>" + httpResponse.status + "</b>" +
-                                      "<br/>CorrelationId: <b>" + httpResponse.data.CorrelationId + "</b>" +
-                                      "<br/>Message: <b> " + httpResponse.data.Message + "</b>" +
-                                      "<br/><br/>Check the <a href=\"#\logviewer\"\">error logs</a> for further information." +
-                                      "</small>";
-
+                        title = httpResponse.statusText;
+                        message = "<small>" +
+                            "<br/>Error code: <b>" + httpResponse.status + "</b>" +
+                            "<br/>CorrelationId: <b>" + httpResponse.data.CorrelationId + "</b>" +
+                            "<br/>Message: <b> " + httpResponse.data.Message + "</b>" +
+                            "<br/><br/>Check the <a href=\"#\logviewer\"\">error logs</a> for further information." +
+                            "</small>";
                         notificationService.displayError(title, message);
                         $rootScope = $injector.get('$rootScope');
                         $location = $injector.get('$location');
