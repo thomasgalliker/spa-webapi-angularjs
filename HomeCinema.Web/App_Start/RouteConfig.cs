6a87431a5c66b35967ddfecc13415e32b09da94f
thomasgalliker <thomas@bluewin.ch>
2017-01-29 20:52:55 +0100

    Ignore favicon.ico in route

--- HomeCinema.Web/App_Start/RouteConfig.cs
+++ HomeCinema.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace HomeCinema.Web
@@ -11,6 +7,7 @@ namespace HomeCinema.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("favicon.ico");
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
